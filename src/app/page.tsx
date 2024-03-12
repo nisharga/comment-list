9bb78375d3087ebcf777a62922e55d670e2a2119
Nisharga <kabir@nexlent.com>
2024-03-12 23:33:53 +0600

    added design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
    <div className="bg-red-500">Hello World</div>
+   <div className="bg-blue-500">Hello World</div>
+   </>
   );
 }
