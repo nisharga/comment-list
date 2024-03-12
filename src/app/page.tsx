24b3937d4c41db3aefe1eb0a6fa32e77c8e854c3
Nisharga <kabir@nexlent.com>
2024-03-13 00:00:03 +0600

    comment box

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
      <div className="bg-[#f5f6fa] h-screen w-screen">
       <div className="container">
         <CommentBox />
+        <div className="bg-red-500">Hello</div>
       </div>
     </div>
   );
