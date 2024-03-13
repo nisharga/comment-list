4c217d31f8bf83d1701d2df3017b199a1b16edd8
Nisharga <kabir@nexlent.com>
2024-03-13 10:29:39 +0600

    comment-list single

--- src/app/Components/CommentList.tsx
+++ src/app/Components/CommentList.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Likes from './Likes'
+import { Plus } from 'lucide-react'
 
 const CommentList = () => {
   return (
     <div className=''>
       Like: <Likes totalLike={10} />
+      <Plus color="red" size={48}/>
     </div>
   )
 }
